21babacfb1037f7c697eca4fbd4c0cf4956e6d13
Joporezka1 <alesha.smolnikoff@gmail.com>
2022-03-21 22:17:13 +0300

    static

--- Siaod4_another/Header.h
+++ Siaod4_another/Header.h
@@ -11,12 +11,17 @@ struct regRow {
 	double total;
 };
 
+//общие функции для статик динамик
 void printTable(regRow* arr,int n);
+void printTableID(regRow* arr, int n);
 regRow fillRow(regRow row);
 
+//статик
+void addRow(regRow* arr, int& n);
+void insert(regRow* arr, int& n);
+void deleteID(regRow* arr, int& n);
 
-
-
+//функции динамик
 void addRowD(regRow* &arr,int &n);
 
 #endif // !_HEADER
--- Siaod4_another/functions.cpp
+++ Siaod4_another/functions.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+const int N = 100;
+
 
 void printTable(regRow* arr, int n) {
 	cout << "ID | Barcode | Quantity | Price | Discount\n";
@@ -10,6 +12,19 @@ void printTable(regRow* arr, int n) {
 	}
 }
 
+void printTableID(regRow* arr, int n) {
+	cout << "¬ведите ID кассы дл€ вывода: \n";
+	int id;
+	cin >> id;
+
+	cout << "ID | Barcode | Quantity | Price | Discount\n";
+	for (auto i = 0; i < n; i++) {
+		if(arr[i].id = id) 
+			cout << arr[i].id << " " << arr[i].barcode << " " << arr[i].quantity << " " << arr[i].price << " " << arr[i].discount << " " << arr[i].total << endl;
+	}
+}
+
+
 regRow fillRow(regRow row) {
 	cout << "¬ведите id кассы, код товара, количество, цену и скидку" << endl;
 	cin >> row.id;
@@ -17,9 +32,54 @@ regRow fillRow(regRow row) {
 	cin >> row.quantity;
 	cin >> row.price;
 	cin >> row.discount;
-	row.total = row.price * row.quantity * (1 + (row.discount / 100));
+	row.total = row.price * row.quantity * (1 - (row.discount / 100));
 	return row;
 }
+
+//==================================STATIC=======================================
+
+void addRow(regRow* arr, int& n) {
+	n++;
+	if (n == N) {
+		return;
+	}
+	arr[n-1] = fillRow(arr[n-1]);
+}
+
+void insert(regRow* arr, int& n) {
+	int id;
+	cout << "¬ведите ID кассы дл€ вставки: \n";
+	cin >> id;
+	regRow temp{};
+	temp = fillRow(temp);
+
+	for (auto i = 0; i < n; i++) {
+		if (arr[i].id == id) {
+			n++;
+			for (auto j = i; j < n; j++) {
+				swap(arr[j], temp);
+			}
+			return;
+		}
+	}
+}
+
+void deleteID(regRow* arr, int& n) {
+	cout<<"¬ведите ID кассы дл€ удалени€: \n";
+	int id;
+	cin >> id;
+	for (auto i = 0; i < n; i++) {
+		if (arr[i].id == id) {
+			n--;
+			for (int j = i; j < n + 1; j++) {
+				swap(arr[j], arr[j + 1]);
+			}
+			i--;
+		}
+	}
+}
+
+//==================================DYNAMIC=======================================
 
 void addRowD(regRow*& arr,int &n) {
 	n++;
@@ -30,4 +90,4 @@ void addRowD(regRow*& arr,int &n) {
 	}
 }
 
-void insert(regRow* &arr,int &n)
+
